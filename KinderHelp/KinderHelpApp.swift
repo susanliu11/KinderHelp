4d837df7f9357a323f1d2f2c8bdc76a9e029e4fe
Susan Liu <susanliu1818@gmail.com>
2023-07-18 10:00:03 -0500

    Initial Commit

---
+++ KinderHelp/KinderHelpApp.swift
@@ -0,0 +1,17 @@
+//
+//  KinderHelpApp.swift
+//  KinderHelp
+//
+//  Created by Scholar on 7/18/23.
+//
+
+import SwiftUI
+
+@main
+struct KinderHelpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
