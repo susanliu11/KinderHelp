d1016bc92cf6666e209ffec39b257b1f000e43e4
Susan Liu <susanliu1818@gmail.com>
2023-07-19 12:52:27 -0500

    7/19

--- KinderHelp/ContentView.swift
+++ KinderHelp/ContentView.swift
@@ -12,7 +12,11 @@ struct ContentView: View {
         NavigationStack {
             
             ZStack{
-                Color(.systemBlue)
+                
+                Image("AppBackground")
+                    .resizable(resizingMode: .stretch)
+                
+                //Color(.systemBlue)
                     .ignoresSafeArea()
                 VStack{
                  Spacer()
@@ -26,26 +30,54 @@ struct ContentView: View {
                         .frame(width: 100)
                     Spacer()
                         .frame(height: 20)
-                   
-                    NavigationLink(destination: CommonSymptoms()) {
-                        Text("Common Symptoms")
-                        .foregroundColor(Color.black) }
+                    ZStack{
+                        //shadow of button
+                        RoundedRectangle(cornerRadius: 100, style: .continuous) .
+                        foregroundColor(Color.init(red: 255, green: 245, blue: 158))
+                            .frame(width: 250, height: 50)
+                        //actual button
+                        RoundedRectangle(cornerRadius: 100, style: .continuous) .
+                        foregroundColor(Color.init(red: 238, green: 244, blue: 212, opacity: 255))
+                            .frame(width: 250, height: 50)
+                        NavigationLink(destination: CommonSymptoms()) {
+                            
+                            
+                            Text("Common Symptoms")
+                                .foregroundColor(Color.black)
+                            
+                        }
+                    }
+                    
                        
                     Spacer()
                         .frame(height: 30)
+                    ZStack{
                         
-                    NavigationLink(destination: PreventativeCare()) {
-                        Text("Preventative Care")
-                        .foregroundColor(Color.black) }
+                        
+                        RoundedRectangle(cornerRadius: 100, style: .continuous) .
+                        foregroundColor(Color.init(red: 255, green: 245, blue: 158))
+                            .frame(width: 250, height: 50)
+                        
+                        NavigationLink(destination: PreventativeCare()) {
+                            Text("Preventative Care")
+                            .foregroundColor(Color.black) }
+                    }
                     Spacer()
                         .frame(height: 30)
-                    NavigationLink(destination: AboutUs()) {
-                        Text("About Us")
-                            .foregroundColor(Color.black)
-                                }
-                            Spacer()
-                                .frame(height: 200)
+                    ZStack{
                         
+                        
+                        RoundedRectangle(cornerRadius: 100, style: .continuous) .
+                        foregroundColor(Color.init(red: 255, green: 245, blue: 158))
+                            .frame(width: 250, height: 50)
+                        NavigationLink(destination: AboutUs()) {
+                            Text("About Us")
+                                .foregroundColor(Color.black)
+                        }
+                    }
+                        Spacer()
+                            .frame(height: 200)
+                    
                         
                         
                     
