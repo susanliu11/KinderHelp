466fcc683a05dee0989ed4020990e7828ea41b6a
Susan Liu <susanliu1818@gmail.com>
2023-07-20 13:17:02 -0500

    common symptoms page done. needs to work on subpages of symptoms.

--- KinderHelp/CommonSymptoms.swift
+++ KinderHelp/CommonSymptoms.swift
@@ -12,97 +12,110 @@ struct CommonSymptoms: View {
         NavigationStack {
             ZStack{
                 Color("teal")
-                    
                     .ignoresSafeArea()
                 
                 VStack {
-                    Text("Symptoms")
+                    Text("Common Symptoms")
                         .font(.largeTitle)
                         .fontWeight(.bold)
                         .padding()
-                    Text("Learn about symptoms frequently presented in sicknesses.Select one to continue.")
-                        .font(.footnote)
-                        .multilineTextAlignment(.center)
-                        .padding()
-                    NavigationLink(destination: Symptom1()) {
-                        Text("Cough")
-                            .font(.body)
-                            .foregroundColor(.black)
-                        .frame(width:160, height:10)
-                        .padding()
-                        }
-                    .background(Rectangle())
-                        .foregroundColor(Color("beige"))
-                        .cornerRadius(30.0)
-                        .padding()
-                    
-                    
-                    NavigationLink(destination: Symptom2()) {
-                        Text("Diarrhea")
-                            .font(.body)
-                            .foregroundColor(.black)
-                            .frame(width:160, height:10)
-                            .padding()
-                    }
-                    .background(Rectangle())
-                        .foregroundColor(Color("beige"))
-                        .cornerRadius(30.0)
-                        .padding()
-                    
-                    NavigationLink(destination: Symptom3()) {
-                        Text("Fever")
-                            .font(.body)
-                            .foregroundColor(.black)
-                            .frame(width:160, height:10)
-                            .padding()
-                    }
-                    .background(Rectangle())
-                        .foregroundColor(Color("beige"))
-                        .cornerRadius(30.0)
-                        .padding()
-                    
-                    NavigationLink(destination: Symptom4()) {
-                        Text("Headache")
-                            .font(.body)
-                            .foregroundColor(.black)
-                            .frame(width:160, height:10)
-                            .padding()
-                    }
-                    .background(Rectangle())
-                        .foregroundColor(Color("beige"))
-                        .cornerRadius(30.0)
-                        .padding()
-                    
-                    NavigationLink(destination: Symptom5()) {
-                        Text("Nausea or Vomiting")
-                            .font(.body)
-                            .foregroundColor(.black)
-                            .frame(width:160, height:10)
-                            .padding()
-                    }
-                    .background(Rectangle())
-                        .foregroundColor(Color("beige"))
-                        .cornerRadius(30.0)
-                        .padding()
-                    
-                    NavigationLink(destination: Symptom6()) {
-                        Text("Sore Throat")
-                            .font(.body)
-                            .foregroundColor(.black)
-                            .frame(width:160, height:10)
-                            .padding()
-                    }
-                    .background(Rectangle())
-                        .foregroundColor(Color("beige"))
-                        .cornerRadius(30.0)
-                        .padding()
-                    
-                    
-                    
-                    //  }
-                    
-                    
+                    Spacer()
                 }
+                    VStack {
+                        VStack{ Text("Learn about symptoms frequently presented in sicknesses.Select one to continue.")
+                                .font(.body)
+                                .multilineTextAlignment(.center)
+                                .padding()
+                        }
+                        .frame(width: 300.0, height: 100.0)
+                        .background(Rectangle().foregroundColor(Color("beige")))
+                        .cornerRadius(5.0)
+                        
+                        
+                        NavigationLink(destination: Symptom1()) {
+                            Text("Cough")
+                                .font(.body)
+                                .foregroundColor(.black)
+                            .frame(width:160, height:10)
+                            .padding()
+                            }
+                        .background(Rectangle())
+                            .foregroundColor(Color("beige"))
+                            .cornerRadius(30.0)
+                            .padding()
+                        
+                        
+                        NavigationLink(destination: Symptom2()) {
+                            Text("Diarrhea")
+                                .font(.body)
+                                .foregroundColor(.black)
+                                .frame(width:160, height:10)
+                                .padding()
+                        }
+                        .background(Rectangle())
+                            .foregroundColor(Color("beige"))
+                            .cornerRadius(30.0)
+                            .padding()
+                        
+                        NavigationLink(destination: Symptom3()) {
+                            Text("Fever")
+                                .font(.body)
+                                .foregroundColor(.black)
+                                .frame(width:160, height:10)
+                                .padding()
+                        }
+                        .background(Rectangle())
+                            .foregroundColor(Color("beige"))
+                            .cornerRadius(30.0)
+                            .padding()
+                        
+                        NavigationLink(destination: Symptom4()) {
+                            Text("Headache")
+                                .font(.body)
+                                .foregroundColor(.black)
+                                .frame(width:160, height:10)
+                                .padding()
+                        }
+                        .background(Rectangle())
+                            .foregroundColor(Color("beige"))
+                            .cornerRadius(30.0)
+                            .padding()
+                        
+                        NavigationLink(destination: Symptom5()) {
+                            Text("Nausea or Vomiting")
+                                .font(.body)
+                                .foregroundColor(.black)
+                                .frame(width:160, height:10)
+                                .padding()
+                        }
+                        .background(Rectangle())
+                            .foregroundColor(Color("beige"))
+                            .cornerRadius(30.0)
+                            .padding()
+                        
+                        NavigationLink(destination: Symptom6()) {
+                            Text("Sore Throat")
+                                .font(.body)
+                                .foregroundColor(.black)
+                                .frame(width:160, height:10)
+                                .padding()
+                        }
+                        .background(Rectangle())
+                            .foregroundColor(Color("beige"))
+                            .cornerRadius(30.0)
+                            .padding()
+                        
+                        
+                        
+                        //  }
+                        
+                        
+                    }
+                    .background(Rectangle().foregroundColor(Color("blue2")))
+                    .cornerRadius(30.0)
+                    
+
+                
             }
             
             
